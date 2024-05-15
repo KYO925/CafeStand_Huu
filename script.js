98c3c16e74cf6834943d298874fbd18f0ce1cf86
KYO925 <kyo925kyo@gmail.com>
2024-05-15 19:19:51 +0900

    ざっくり

---
+++ script.js
@@ -0,0 +1,17 @@
+function toggleMenu() {
+    var menu = document.getElementById("menu");
+    if (menu.style.display === "block") {
+        menu.style.display = "none";
+    } else {
+        menu.style.display = "block";
+    }
+}
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
